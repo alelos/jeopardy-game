1f7ab546f7554eaa0d4cd0aa778b6bfd2493d5ce
alelos <aleksandra.los@knowit.se>
2022-11-02 19:37:41 +0100

    add flipCard function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const game= document.getElementById('game')
+const game = document.getElementById('game')
 const score = document.getElementById('score')
 
 const gameCategories = [
@@ -7,8 +7,8 @@ const gameCategories = [
         questions: [
             {
                 question: 'What is profit?',
-                answer: ['The reward an entrepreneur (business owner) earn for taking the RISK of producing.', 
-                'The loan an entrepreneur (business owner) taking'],
+                answer: ['The reward an entrepreneur (business owner) earn for taking the RISK of producing.',
+                    'The loan an entrepreneur (business owner) taking'],
                 correct: 'The reward an entrepreneur (business owner) earn for taking the RISK of producing.',
                 level: 'hard'
             }
@@ -49,7 +49,7 @@ const gameCategories = [
     },
     {
         genre: 'Geography',
-        questions: [        {
+        questions: [{
             question: 'What is the Amazon?',
             answer: ['This river is the longest in the world..', 'This is the only country that is also a continent.'],
             correct: 'This river is the longest in the world.',
@@ -58,7 +58,7 @@ const gameCategories = [
     },
     {
         genre: 'Random',
-        questions: [        {
+        questions: [{
             question: 'What number is the Roman numeral XVI?',
             answer: ['61.', '16'],
             correct: '16.',
@@ -66,3 +66,61 @@ const gameCategories = [
         }]
     },
 ]
+
+function addCategory(category) {
+    const column = document.createElement('div')
+    column.classList.add('genre-column')
+
+    const genreTitle = document.createElement('div')
+    genreTitle.classList.add('genre-title')
+    genreTitle.innerHTML = category.genre
+    column.append(genreTitle)
+    game.append(column)
+
+    category.questions.forEach(question => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        column.append(card)
+        if (question.level === 'easy') {
+            card.innerHTML = 100
+        }
+        if (question.level === 'medium') {
+            card.innerHTML = 200
+        }
+        if (question.level === 'hard') {
+            card.innerHTML = 300
+        }
+
+        card.setAttribute('data-question', question.question)
+        card.setAttribute('data-answer-1', question.answer[0])
+        card.setAttribute('data-answer-2', question.answer[1])
+        card.setAttribute('data-correct', question.correct)
+        card.setAttribute('data-value', card.getInnerHTML())
+
+        card.addEventListener('click', flipCard)
+
+    })
+
+
+}
+
+gameCategories.forEach(category => addCategory(category))
+
+function flipCard() {
+    this.innerHTML = ""
+    this.style.fontSize = '15px'
+    this.style.lineHeight = '30px'
+    const textDisplay = document.createElement('div')
+    textDisplay.classList.add('card-text')
+    textDisplay.innerHTML= this.getAttribute('data-question')
+
+    const firstBtn = document.createElement('button')
+    firstBtn.classList.add('first-btn')
+    const secondBtn = document.createElement('button')
+    secondBtn.classList.add('second-btn')
+
+    firstBtn.innerHTML = this.getAttribute('data-answer-1')
+    secondBtn.innerHTML = this.getAttribute('data-answer-2')
+
+    this.append(textDisplay, firstBtn, secondBtn)
+}
